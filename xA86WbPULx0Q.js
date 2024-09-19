467f11c0935c857ffbc5cf73656a5da19ad9b861
Jazzdar1 <darajazb@gmail.com>
2024-09-19 08:52:37 +0530

    Add files via upload

---
+++ xA86WbPULx0Q.js
@@ -0,0 +1,516 @@
+const jsonData = {
+  /*    "049": {
+      	"url": "049",
+      	"name": "SSC Sports Extra 1",
+      	"logo": "https://xDdDAYemk349.io/HpLHKZv.png",
+      	"group": "Sports"
+      },
+      "050": {
+      	"url": "050",
+      	"name": "SSC Sports Extra 2",
+      	"logo": "https://xDdDAYemk349.io/HpLHqnR.png",
+      	"group": "Sports"
+      },
+      "051": {
+      	"url": "051",
+      	"name": "SSC Sports Extra 3",
+      	"logo": "https://xDdDAYemk349.io/HpLHBGp.png",
+      	"group": "Sports"
+      },
+      "052": {
+      	"url": "052",
+      	"name": "DAZN 1",
+      	"logo": "https://xDdDAYemk349.io/HpLlhvI.png",
+      	"group": "Sports"
+      },
+      "053": {
+      	"url": "053",
+      	"name": "DAZN 2",
+      	"logo": "https://xDdDAYemk349.io/HpLlXYN.png",
+      	"group": "Sports"
+      },
+      "054": {
+      	"url": "054",
+      	"name": "ZONA DAZN",
+      	"logo": "https://xDdDAYemk349.io/HpLlW2p.png",
+      	"group": "Sports"
+      },
+      "064": {
+      	"url": "064",
+      	"name": "DAZN La Liga",
+      	"logo": "https://xDdDAYemk349.io/HpQyYzJ.png",
+      	"group": "Sports"
+      },
+      
+      "055": {
+      	"url": "055",
+      	"name": "BEIN SPORTS 1",
+      	"logo": "https://xDdDAYemk349.io/HpLlECJ.png",
+      	"group": "Sports"
+      },
+      "056": {
+      	"url": "056",
+      	"name": "BEIN SPORTS 2",
+      	"logo": "https://xDdDAYemk349.io/HpLl0Qa.png",
+      	"group": "Sports"
+      },
+      "063": {
+      	"url": "063",
+      	"name": "BEIN SPORTS 3",
+      	"logo": "https://xDdDAYemk349.io/HpLlGEv.png",
+      	"group": "Sports"
+      },
+      
+      "057": {
+      	"url": "057",
+      	"name": "Inter Milan TV",
+      	"logo": "https://xDdDAYemk349.io/HpLmnAG.png",
+      	"group": "Sports"
+      },
+      "058": {
+      	"url": "058",
+      	"name": "AC Milan TV",
+      	"logo": "https://xDdDAYemk349.io/HpLmC9s.png",
+      	"group": "Sports"
+      },
+      "062": {
+      	"url": "062",
+      	"name": "Play Sports 1",
+      	"logo": "https://xDdDAYemk349.io/HpQ6AR2.png",
+      	"group": "Sports"
+      },
+      "059": {
+      	"url": "059",
+      	"name": "EURO SPORTS 1",
+      	"logo": "https://xDdDAYemk349.io/HpZ3vrx.png",
+      	"group": "Sports"
+      },
+      "060": {
+      	"url": "060",
+      	"name": "EURO SPORTS 2",
+      	"logo": "https://xDdDAYemk349.io/HpZ3SdQ.png",
+      	"group": "Sports"
+      },
+      "061": {
+      	"url": "061",
+      	"name": "CRICLIFE 3",
+      	"logo": "https://xDdDAYemk349.io/HpLmqSn.png",
+      	"group": "Sports"
+      },
+          "": {
+    "url": "",
+    "name": "",
+    "logo": "",
+    "group": ""
+  },
+*/
+
+  "jiocinema": {
+    "url": "https://special.cricktv.site/jiocinema",
+    "name": "JioCinema Web",
+    "logo": "https://xDdDAYemk349.io/d5yeaGS.md.png",
+    "group": "Website"
+  },
+  "fancode": {
+    "url": "https://special.cricktv.site/fancode",
+    "name": "Fancode Web",
+    "logo": "https://xDdDAYemk349.io/d5yvMLQ.md.png",
+    "group": "Website"
+  },
+  "cl2": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=cl2",
+    "name": "CRICLIFE 2",
+    "logo": "https://xFI9QvmXB4uz.co/jDyn9fS/Picsart-24-07-27-01-42-37-317.png",
+    "group": "Sports"
+  },
+
+  "willow": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=willow",
+    "name": "Willow SD",
+    "logo": "https://xFI9QvmXB4uz.co/Js5t9dM/Picsart-24-07-27-01-20-13-863.png",
+    "group": "Sports"
+  },
+  "willowHD": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=willow2",
+    "name": "Willow HD",
+    "logo": "https://xFI9QvmXB4uz.co/25LyH5D/20240727-041020.png",
+    "group": "Sports"
+  },
+
+  "astro": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=astro2",
+    "name": "Astro Cricket",
+    "logo": "https://xDdDAYemk349.io/HpsO1kb.png",
+    "group": "Sports"
+  },
+  "asports": {
+    "url": "https://EjtV3huI0Kms.site/tv?id=asports",
+    "name": "A Sports Cricket",
+    "logo": "https://xFI9QvmXB4uz.co/Cmz5VJ6/20240727-020613.png",
+    "group": "Sports"
+  },
+  "tc": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=tc",
+    "name": "Ten Cricket",
+    "logo": "https://xDdDAYemk349.io/dXNk3f1.md.png",
+    "group": "Sports"
+  },
+  "ptvs": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ptvs",
+    "name": "PTV SPORTS",
+    "logo": "https://xFI9QvmXB4uz.co/2q6qJmy/20240727-042215.png",
+    "group": "Sports"
+  },
+  "tnt1": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=tnt1",
+    "name": "TNT SPORTS 1",
+    "logo": "https://xDdDAYemk349.io/dIH66J4.png",
+    "group": "Sports"
+  },
+  "tnt2": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=tnt2",
+    "name": "TNT SPORTS 2",
+    "logo": "https://xDdDAYemk349.io/dIH6rgf.png",
+    "group": "Sports"
+  },
+  "tnt3": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=tnt3",
+    "name": "TNT SPORTS 3",
+    "logo": "https://xDdDAYemk349.io/dIH68sn.png",
+    "group": "Sports"
+  },
+  "tnt4": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=tnt4",
+    "name": "TNT SPORTS 4",
+    "logo": "https://xDdDAYemk349.io/dIH6g0G.png",
+    "group": "Sports"
+  },
+  "tnt5": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=tnt5",
+    "name": "TNT SPORTS 5",
+    "logo": "https://xDdDAYemk349.io/dIH6Ufs.png",
+    "group": "Sports"
+  },
+  "sports18": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=sports18",
+    "name": "Sports 18-1",
+    "logo": "https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,dpr_6.75,w_40/https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/imageContent-70046-l21cybww-v2/imageContent-70046-l21cybww-m5.png",
+    "group": "Sports"
+  },
+  "sports18hd": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=sports18hd",
+    "name": "Sports 18-1 HD",
+    "logo": "https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,dpr_6.75,w_40/https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/imageContent-74232-la6z41u8-v3/imageContent-74232-la6z41u8-m5.png",
+    "group": "Sports"
+  },
+  "skysports": {
+    "url": "https://EjtV3huI0Kms.site/tv?id=skysports",
+    "name": "Sky Sports Cricket",
+    "logo": "https://xFI9QvmXB4uz.co/PCXT9Sz/Picsart-24-07-27-01-28-16-930.png",
+    "group": "Sports"
+  },
+  "skysportscricket": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportscricket",
+    "name": "Sky Sports Cricket",
+    "logo": "https://xDdDAYemk349.io/dRwm9ou.md.png",
+    "group": "Sports"
+  },
+  "skysportsmainevent": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsmainevent",
+    "name": "Sky Sports Mainevent",
+    "logo": "https://xDdDAYemk349.io/dRwbpte.md.png",
+    "group": "Sports"
+  },
+  "skysportsmix": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsmix",
+    "name": "Sky Sports Mix",
+    "logo": "https://xDdDAYemk349.io/dRwbbA7.md.png",
+    "group": "Sports"
+  },
+  "skysportsaction": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsaction",
+    "name": "Sky Sports Action",
+    "logo": "https://xDdDAYemk349.io/dRwmHMb.md.png",
+    "group": "Sports"
+  },
+  "skysportsfootball": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsfootball",
+    "name": "Sky Sports Football",
+    "logo": "https://xDdDAYemk349.io/dRwm2Kx.md.png",
+    "group": "Sports"
+  },
+  "skysportspremierleague": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportspremierleague",
+    "name": "Sky Sports Premier League",
+    "logo": "https://xDdDAYemk349.io/dRwm3cQ.md.png",
+    "group": "Sports"
+  },
+  "skysportsarena": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsarena",
+    "name": "Sky Sports Arena",
+    "logo": "https://xDdDAYemk349.io/dRwmqAP.md.png",
+    "group": "Sports"
+  },
+  "skysportsgolf": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsgolf",
+    "name": "Sky Sports Golf",
+    "logo": "https://xDdDAYemk349.io/dRwmTKv.md.png",
+    "group": "Sports"
+  },
+  "skysportsf1": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=skysportsf1",
+    "name": "Sky Sports F1",
+    "logo": "https://xDdDAYemk349.io/dRwmziJ.md.png",
+    "group": "Sports"
+  },
+
+  "044": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ssc1",
+    "name": "SSC Sports 1",
+    "logo": "https://xDdDAYemk349.io/HpLHT9n.png",
+    "group": "Sports"
+  },
+  "045": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ssc2",
+    "name": "SSC Sports 2",
+    "logo": "https://xDdDAYemk349.io/HpLHxat.png",
+    "group": "Sports"
+  },
+  "046": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ssc3",
+    "name": "SSC Sports 3",
+    "logo": "https://xDdDAYemk349.io/HpLHoFI.png",
+    "group": "Sports"
+  },
+  "047": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ssc4",
+    "name": "SSC Sports 4",
+    "logo": "https://xDdDAYemk349.io/HpLHFwJ.png",
+    "group": "Sports"
+  },
+  "048": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=ssc5",
+    "name": "SSC Sports 5",
+    "logo": "https://xDdDAYemk349.io/HpLAKDx.png",
+    "group": "Sports"
+  },
+    "spotv": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=willow",
+    "name": "SPOTV",
+    "logo": "https://xFI9QvmXB4uz.co/KGK6MNF/Picsart-24-07-27-01-35-56-981.png",
+    "group": "Sports"
+  },
+  "beinsports1": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=beinsports1",
+    "name": "Bein Sports 1",
+    "logo": "https://xDdDAYemk349.io/dXNG2zg.md.png",
+    "group": "Football"
+  },
+  "epl1": {
+    "url": "https://EjtV3huI0Kms.site/dtv-hindi2?id=epl1",
+    "name": "Premier League",
+    "logo": "https://xDdDAYemk349.io/dXNW8T7.md.png",
+    "group": "Football"
+  }
+  /* "olympic1": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic1",
+     "name": "Olympics Channel 1",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic2": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic2",
+     "name": "Olympics Channel 1",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic3": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic3",
+     "name": "Olympics Channel 2",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic4": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic4",
+     "name": "Olympics Channel 2",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic5": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic5",
+     "name": "Olympics Channel 3",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic6": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic6",
+     "name": "Olympics Channel 3",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic7": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic7",
+     "name": "Olympics Channel 4",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic8": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic8",
+     "name": "Olympics Channel 4",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic9": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic9",
+     "name": "Olympics Channel 5",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic10": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic10",
+     "name": "Olympics Channel 5",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic11": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic11",
+     "name": "Olympics Channel 6",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic12": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic12",
+     "name": "Olympics Channel 6",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic13": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic13",
+     "name": "Olympics Channel 7",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic14": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic14",
+     "name": "Olympics Channel 7",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic15": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic15",
+     "name": "Olympics Channel 8",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic16": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic16",
+     "name": "Olympics Channel 8",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic17": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic17",
+     "name": "Olympics Channel 9",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic18": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic18",
+     "name": "Olympics Channel 9",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic19": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic19",
+     "name": "Olympics Channel 10",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic20": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic20",
+     "name": "Olympics Channel 10",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic21": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic21",
+     "name": "Olympics Channel 11",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic22": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic22",
+     "name": "Olympics Channel 11",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic23": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic23",
+     "name": "Olympics Channel 12",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic24": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic24",
+     "name": "Olympics Channel 12",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic25": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic25",
+     "name": "Olympics Channel 13",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic26": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic26",
+     "name": "Olympics Channel 13",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic27": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic27",
+     "name": "Olympics Channel 14",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic28": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic28",
+     "name": "Olympics Channel 14",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic29": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic29",
+     "name": "Olympics Channel 15",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic30": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic30",
+     "name": "Olympics Channel 15",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   },
+   "olympic31": {
+     "url": "https://EjtV3huI0Kms.site/dtv-hindi?id=olympic31",
+     "name": "Olympics Channel 16",
+     "logo": "https://xDdDAYemk349.io/du7WQKg.png",
+     "group": "Olympics"
+   }
+
+
+     
+           "skysports2": {
+             "url": "skysports",
+             "name": "Sky Sports Pl",
+             "logo": "https://xDdDAYemk349.io/HpsO1kb.png",
+             "group": "Sports"
+           },
+           "": {
+             "url": "",
+             "name": "",
+             "logo": "",
+             "group": "Sports"
+           },*/
+};
